c449458f755f3b8d98904e0aa932497706002c32
aguduranti <agu.duranti@gmail.com>
2025-09-09 21:56:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ const body = document.body;
 // Cargar el modo oscuro desde localStorage
 if(localStorage.getItem('darkMode') === 'enabled'){
   body.classList.add('dark');
+  toggle.textContent = '☀️';
+} else {
+  toggle.textContent = '🌙';
 }
 
-// Cambiar modo oscuro
+// Cambiar modo oscuro y actualizar ícono
 toggle.addEventListener('click', () => {
   body.classList.toggle('dark');
   if(body.classList.contains('dark')){
     localStorage.setItem('darkMode', 'enabled');
+    toggle.textContent = '☀️';
   } else {
     localStorage.setItem('darkMode', 'disabled');
+    toggle.textContent = '🌙';
   }
 });
